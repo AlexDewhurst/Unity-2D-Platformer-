b6c4d0556f12f9a636b5010c070e6b042a787c1b
Dewhurst_Alex <dewha618@hsd.k12.or.us>
2018-10-19 13:20:50 -0700

    Added script for basic horizontal movement to player object

--- Assets/C# Scripts/GameController.cs
+++ Assets/C# Scripts/GameController.cs
@@ -3,14 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameController : MonoBehaviour {
+    private Rigidbody m_rb;
+
 
 	// Use this for initialization
 	void Start () {
-		
+        m_rb = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    private void FixedUpdate()
+    {
+        /* Get user input to add a left-right force (horizontal) to our player object
+        */
+        //get user input
+        float movement = Input.GetAxis("Horizontal");
+        //add force
+        m_rb.AddForce(new Vector3(movement, 0.0F, 0.0F));
+    }
 }
